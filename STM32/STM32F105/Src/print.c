b766c7299fb4c093b19d2bb0b8f459f5e45f5dfd
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-01-13 11:41:31 +0100

    Serial debug print.

---
+++ STM32/STM32F105/Src/print.c
@@ -0,0 +1,87 @@
+#include "stm32f1xx_hal.h"
+
+#include <stdio.h>
+
+#include "print.h"
+
+extern UART_HandleTypeDef huart1;
+
+void printchar(char c)
+{
+	HAL_UART_Transmit(&huart1, (unsigned char*)&c, 1, 1000);
+}
+
+void printhex(unsigned char c)
+{	
+	unsigned char c1;
+
+	c1=c>>4;
+
+	if(c1<10)
+	{
+		printchar('0'+c1);
+	}
+	else
+	{
+		printchar('A'+(c1-10));
+	}
+
+	c1=c&0xF;
+
+	if(c1<10)
+	{
+		printchar('0'+c1);
+	}
+	else
+	{
+		printchar('A'+(c1-10));
+	}
+}
+
+void printhex_long(unsigned long c)
+{
+	printhex((c>>24)&0xFF);
+	printhex((c>>16)&0xFF);
+	printhex((c>>8)&0xFF);
+	printhex(c&0xFF);
+}
+
+void print(const char *pucBuffer)
+{
+	int i;
+    //
+    // Loop while there are more characters to send.
+    //
+	i=0;
+    while(pucBuffer[i])
+    {
+		printchar(pucBuffer[i]);
+        i++;
+    }
+}
+
+void printdec(unsigned char c)
+{
+	unsigned char c1,c2,c3;
+
+	c1=c/100;
+	printchar('0'+c1);
+	c2=(c/10) - (c1*10);
+	printchar('0'+c2);
+	c3= c - ( (c1*100) + (c2*10) );
+	printchar('0'+c3);
+}
+
+void printbuf(unsigned char * buf,int size)
+{
+	int i;
+	print("\r\n");
+	for(i=0;i<size;i++)
+	{
+		if(!(i&0xF))
+			print("\r\n");
+
+		printhex(buf[i]);
+	}
+	print("\r\n");
+}
---
+++ STM32/STM32F105/Src/print.h
@@ -0,0 +1,6 @@
+void printchar(char c);
+void print(const char *pucBuffer);
+void printdec(unsigned char c);
+void printhex(unsigned char c);
+void printhex_long(unsigned long c);
+void printbuf(unsigned char * buf,int size);
