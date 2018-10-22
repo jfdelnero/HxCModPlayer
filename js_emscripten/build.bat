b85949496dc790e8753c1baa2882553b12d9a157
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-10-22 22:18:00 +0200

    Remove old build scripts.

--- js_emscripten/build.bat
+++
@@ -1,4 +0,0 @@
-call emcc.bat -Wno-pointer-sign -I./ -I../ -Os -O3 --llvm-lto 1 -s NO_FILESYSTEM=1 js_hxcmod_player.c ../hxcmod.c -s EXPORTED_FUNCTIONS="['_malloc','_free','_loadMod','_getNextSoundData','_unloadMod']" -o js_hxcmod_player.html
-copy js_hxcmod_player.js .\www
-
-
--- js_emscripten/build.sh
+++
@@ -1,7 +0,0 @@
-rm *.bc *.wasm *.htm *.js
-emcc -I./ -I../ -O3 ../hxcmod.c -o hxcmod.bc
-emcc -I./ -I../ -O3 js_hxcmod_player.c -o js_hxcmod_player.bc
-emcc hxcmod.bc js_hxcmod_player.bc -s EXPORTED_FUNCTIONS="['_malloc','_free','_loadMod','_getNextSoundData','_unloadMod','_main']" -o js_hxcmod_player.js
-cp js_hxcmod_player.js ./www
-cp js_hxcmod_player.wasm ./www
-
